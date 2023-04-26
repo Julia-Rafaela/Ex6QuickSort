04e4ee4e8a88fb53e88b4b0781e70f76f622a722
Julia-Rafaela <124639320+Julia-Rafaela@users.noreply.github.com>
2023-04-25 22:19:38 -0300

    commit

---
+++ src/view/Ex6.java
@@ -0,0 +1,22 @@
+package view;
+
+import br.com.juliarafaela.QuickSort.ordenacao;
+
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		int vetor[]= {69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1};
+
+		ordenacao o = new ordenacao();
+		vetor = o.quickSort(vetor, 0, vetor.length -1);
+		
+		for(int valor: vetor) {
+			System.out.print(valor);
+			System.out.println(" ");
+		}
+		
+		
+	}
+
+}
